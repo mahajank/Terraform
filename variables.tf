f341db0d7ce04a75ca5e5b61a2a9e30adf334d2b
mahajank <karan.mahajan.inc@gmail.com>
2018-01-29 02:23:46 +0000

    Updated Variables

---
+++ variables.tf
@@ -0,0 +1,13 @@
+
+variable "client_id" {
+  description = "client_id"
+}
+variable "client_secret" {
+  description = "client_secret"
+}
+variable "subscription_id" {
+  description = "subscription_id"
+}
+variable "tenant_id" {
+  description = "tenant_id"
+}
