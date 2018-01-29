86a8bc73026da7edf63ad7e14c4d91de7d381145
root <root@Terraform.50wabwh1v5quze3utagatvgdha.bx.internal.cloudapp.net>
2018-01-29 02:00:16 +0000

    Demo Testing

---
+++ demo-network.tf
@@ -0,0 +1,28 @@
+# Create a resource group
+resource "azurerm_resource_group" "network" {
+  name     = "Demo"
+  location = "East US"
+}
+
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "network" {
+  name                = "Development-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "${azurerm_resource_group.network.location}"
+  resource_group_name = "${azurerm_resource_group.network.name}"
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  subnet {
+    name           = "subnet3"
+    address_prefix = "10.0.3.0/24"
+  }
+}
